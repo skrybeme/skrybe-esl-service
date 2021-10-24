d481845bf82e01448bd91737c3aa92e8556e1d11
eatthatpie <eatthatpie.dev@gmail.com>
2021-10-24 11:54:35 +0200

    refactor: update email messages

--- src/infra/email-service/messages.ts
+++ src/infra/email-service/messages.ts
@@ -4,7 +4,7 @@ export const getConfirmationEmailMessageHtml = (unsubscribeUrl: string) => `
 <p>We will use this email to notify you when we publish the full version of Skrybe.co.</p>
 <p><small><strong>If you want to unsubscribe</strong></small></p>
 <p><small>Click <a href="${unsubscribeUrl}">this link</a> if you want to unsubscribe. We will remove your email from our database. Please note, that this operation is irreversible.</small></p>
-<p>All best,<br/>Tony</p>
+<p>All best,<br/>Tom</p>
 `;
 
 export const getConfirmationEmailMessageText = (unsubscribeUrl: string) => `
@@ -17,5 +17,5 @@ We will use this email to notify you when we publish the full version of Skrybe.
 Click ${unsubscribeUrl} if you want to unsubscribe. We will remove your email from our database. Please note, that this operation is irreversible.
 
 All best,
-Tony
+Tom
 `;
